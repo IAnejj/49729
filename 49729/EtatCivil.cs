df073e7f6c11bb4cde7ee467cbbbe006dee49fb3
Imad <Imad@DESKTOP-MMG1R1D>
2016-10-30 00:02:49 +0200

    Ajout constructeur

--- 49729/EtatCivil.cs
+++ 49729/EtatCivil.cs
@@ -8,9 +8,37 @@ namespace _49729
 {
     class EtatCivil
     {
-        string nom;
-        string prenom;
-        string statut_marital;//utiliser un int ?
-        int nb_enfants;
+        private string nom;
+        public string Nom
+        {
+            get { return nom; }
+        }
+        private string prenom;
+        public string Prenom
+        {
+            get { return prenom; }
+        }
+        private string statut_martial;
+        public string Statut_martial
+        {
+            get { return statut_martial; }
+        }
+        private int nb_enfants;
+        public int Nb_enfants
+        {
+            get { return nb_enfants; }
+        }
+
+
+        public EtatCivil(string nom, string prenom, string statut_martial, int nb_enfants)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.statut_martial = statut_martial;
+            this.nb_enfants = nb_enfants;
+        }
+
+
+
     }
 }
--- 49729/Program.cs
+++ 49729/Program.cs
@@ -10,6 +10,15 @@ namespace _49729
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Entrez votre nom");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Entrez votre prenom");
+            string prenom = Console.ReadLine();
+            Console.WriteLine("Entrez votre statut martial");
+            string smartial = Console.ReadLine();
+            Console.WriteLine("Entrez votre nombre d'enfants");
+            int nbenfants = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
         }
     }
 }
